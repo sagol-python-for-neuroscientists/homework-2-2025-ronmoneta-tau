d5ad6f8055356adf68d689bff123ba9ca827cbe1
Ron Moneta <ronmoneta@mail.tau.ac.il>
2025-04-08 20:01:39 +0300

    Finished q2

--- hw2_q2.py
+++ hw2_q2.py
@@ -1,8 +1,32 @@
 from collections import namedtuple
 from enum import Enum
+import itertools
 
 Condition = Enum("Condition", ("CURE", "HEALTHY", "SICK", "DYING", "DEAD"))
 Agent = namedtuple("Agent", ("name", "category"))
+
+
+def apply_cure(agent: Agent) -> Agent:
+    """
+    Applies cure to an agent - if he is not Cure himself
+    parameter agent: Agent
+    return: Updated Agent
+    """
+    if agent.category != Condition.CURE:
+        agent = Agent(agent.name, Condition(agent.category.value - 1))
+
+    return agent
+
+
+def apply_worsening(agent: Agent) -> Agent:
+    """
+    Applies worsening to an agent
+    parameter agent: Agent
+    return: Updated Agent
+    """
+    agent = Agent(agent.name, Condition(agent.category.value + 1))
+
+    return agent
 
 
 def meetup(agent_listing: tuple) -> list:
@@ -29,3 +53,27 @@ def meetup(agent_listing: tuple) -> list:
         A list of Agents with their 'category' field changed according to the result
         of the meeting.
     """
+    updated_agents = []
+
+    discarded_categories = (Condition.HEALTHY, Condition.DEAD)
+    ineligible = [agent for agent in agent_listing if agent.category in discarded_categories]
+    eligible = [agent for agent in agent_listing if agent.category not in discarded_categories]
+
+    for agent1, agent2 in itertools.zip_longest(*[iter(eligible)] * 2, fillvalue=None):
+        # If there's an agent left without a partner, it remains unchanged.
+        if agent2 is None:
+            updated_agents.append(agent1)
+            continue
+
+        # handle the case when at least one agent is a Cure.
+        if agent1.category == Condition.CURE or agent2.category == Condition.CURE:
+            agent1 = apply_cure(agent1)
+            agent2 = apply_cure(agent2)
+
+        else:
+            agent1 = apply_worsening(agent1)
+            agent2 = apply_worsening(agent2)
+
+        updated_agents.extend([agent1, agent2])
+
+    return updated_agents + ineligible
